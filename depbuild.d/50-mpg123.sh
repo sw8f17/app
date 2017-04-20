097f1eab9fe4d5033bb751ca770af476b1feab2a
Mathias Sass Michno <mmichn13@student.aau.dk>
2017-04-20 11:26:05 +0200

    unix master race fix

--- depbuild.d/50-mpg123.sh
+++ depbuild.d/50-mpg123.sh
@@ -3,7 +3,7 @@
 mkdir "mpg123"
 cd "mpg123"
 
-$package_dir/mpg123/configure --host=$triple --build=i686-pc-linux-gnu --prefix=$install_dir CC=$toolchain_dir/bin/clang CFLAGS=-DHAVE_MMAP=1 --disable-modules
+$package_dir/mpg123/configure --host=$triple --build=i686-apple-darwin --prefix=$install_dir CC=$toolchain_dir/bin/clang CFLAGS=-DHAVE_MMAP=1 NM=$toolchain_dir/bin/arm-linux-androideabi-nm AR=$toolchain_dir/bin/arm-linux-androideabi-ar RANLIB=$toolchain_dir/bin/arm-linux-androideabi-ranlib --disable-modules
 
 make
 
--- mkdep.sh
+++ mkdep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+# set -e
 
 function clean_up {
 	exit
