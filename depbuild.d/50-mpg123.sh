96508dbcb41390623da2840efb1ce9f9a318489f
Jesper Jensen <jesper@slashwin.dk>
2017-04-20 13:05:04 +0200

    Added selective compilation

--- depbuild.d/50-mpg123.sh
+++ depbuild.d/50-mpg123.sh
@@ -3,7 +3,15 @@
 mkdir "mpg123"
 cd "mpg123"
 
-$package_dir/mpg123/configure --host=$triple --build=i686-apple-darwin --prefix=$install_dir CC=$toolchain_dir/bin/clang CFLAGS=-DHAVE_MMAP=1 NM=$toolchain_dir/bin/arm-linux-androideabi-nm AR=$toolchain_dir/bin/arm-linux-androideabi-ar RANLIB=$toolchain_dir/bin/arm-linux-androideabi-ranlib --disable-modules
+$package_dir/mpg123/configure \
+	--host=$triple \
+	--prefix=$install_dir \
+	CC=$toolchain_dir/bin/clang \
+	CFLAGS=-DHAVE_MMAP=1 \
+	NM=$toolchain_dir/bin/${toolchain_prefix}nm \
+	AR=$toolchain_dir/bin/${toolchain_prefix}ar \
+	RANLIB=$toolchain_dir/bin/${toolchain_prefix}ranlib \
+	--disable-modules
 
 make
 
--- mkdep.sh
+++ mkdep.sh
@@ -1,6 +1,114 @@
 #!/bin/bash
 # set -e
 
+##START OF CONFIG
+
+abis=(
+	"armeabi"
+	"armeabi-v7a"
+	"arm64-v8a"
+	"x86"
+	"x86_64"
+	"mips"
+	"mips64"
+)
+
+triples=(
+	"arm-none-linux-androideabi"
+	"armv7-none-linux-androideabi"
+	"aarch64-none-linux-androideabi"
+	"x86-none-linux-androideabi"
+	"x86_64-none-linux-androideabi"
+	"mips-none-linux-androideabi"
+	"mips64-none-linux-androideabi"
+)
+
+toolchains=(
+	"arm"
+	"arm"
+	"arm64"
+	"x86"
+	"x86_64"
+	"mips"
+	"mips64"
+)
+
+toolchain_prefixes=(
+	"arm-linux-androideabi-"
+	"arm-linux-androideabi-"
+	"aarch64-linux-android-"
+	"i686-linux-android-"
+	"x86_64-linux-android-"
+	"mipsel-linux-android-"
+	"mips64el-linux-android-"
+)
+
+##END OF CONFIG
+
+get_abi_index() {
+	local i
+	for((i = 0; i < ${#abis[@]}; i++)); do
+		if [ "$1" == "${abis[i]}" ]; then
+			echo "$i"
+			break
+		fi
+	done
+}
+
+getopt --test > /dev/null
+if [[ $? -ne 4 ]]; then
+    echo "You have the wrong getopt, getoptta here."
+    exit 1
+fi
+
+SHORT_OPT=t:
+LONG_OPT=target:
+
+# -temporarily store output to be able to check for errors
+# -activate advanced mode getopt quoting e.g. via “--options”
+# -pass arguments only via   -- "$@"   to separate them correctly
+PARSED=$(getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 2
+fi
+eval set -- "$PARSED"
+
+targets=()
+while true; do
+    case "$1" in
+        -t|--target)
+			targets+=("$2")
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error"
+            exit 3
+            ;;
+    esac
+done
+
+stages=()
+if [ ${#targets[@]} -eq 0 ]; then
+    echo "Compiling everything"
+	for((i = 0; i < ${#abis[@]}; i++)); do
+		stages+=($i)
+	done
+else
+	for target in ${targets[@]}; do
+		i=$(get_abi_index "$target")
+		if [ "$i" == "" ]; then
+			echo "Target \"$target\" not found"
+			exit 1
+		fi
+		stages+=("$i")
+	done
+	echo "Compiling selectively"
+fi
+
 function clean_up {
 	exit
 }
@@ -35,36 +143,6 @@ cd "$build_dir"
 #Now we are ready to compile
 #Remember to create a new directory for every package, and install into the install dir
 
-abis=(
-	"armeabi"
-	"armeabi-v7a"
-	"arm64-v8a"
-	"x86"
-	"x86_64"
-	"mips"
-	"mips64"
-)
-
-triples=(
-	"arm-none-linux-androideabi"
-	"armv7-none-linux-androideabi"
-	"aarch64-none-linux-androideabi"
-	"x86-none-linux-androideabi"
-	"x86_64-none-linux-androideabi"
-	"mips-none-linux-androideabi"
-	"mips64-none-linux-androideabi"
-)
-
-toolchains=(
-	"arm"
-	"arm"
-	"arm64"
-	"x86"
-	"x86_64"
-	"mips"
-	"mips64"
-)
-
 package() {
 	file="$1"
 	mkdir -p "$jniLibs/$abi/"
@@ -74,14 +152,16 @@ package() {
 export -f package
 
 #I want to build for everything, so lets do that
-steps=${#abis[@]}
+steps=${#stages[@]}
 for((i = 0; i < steps; i++)); do
 	step=$((i+1))
+	index=${stages[i]}
 
-	export abi=${abis[i]}
-	export triple=${triples[i]}
-	export toolchain=${toolchains[i]}
+	export abi=${abis[index]}
+	export triple=${triples[index]}
+	export toolchain=${toolchains[index]}
 	export toolchain_dir="$base/toolchain/$toolchain"
+	export toolchain_prefix=${toolchain_prefixes[index]}
 
 	status_line="[$step/$steps] Building for $abi"
 	echo -e '\033]2;'$status_line'\007'
